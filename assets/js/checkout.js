e6f608fe250ec59c68af0287e3d156fb6162725a
Kervens Dorvilus <dorviluskervens359@gmail.com>
2026-03-24 21:15:21 -0400

    Add backend practice data import to checkout.js

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,6 +1,7 @@
 import { initHeader } from '../js/header.js';
 import { renderCart } from './checkout/orderSummary.js';
 //import '../data/cartDataClass.js'
+import '../data/backendPractice.js'
 
 initHeader();
 renderCart();
